41989ef90e8512ef80596aafc9aff1b699e94192
Ritik Raj <raj769417@gmail.com>
2024-07-15 00:40:48 +0530

    feat: added endpoint to fetch trending polls

--- src/redux/api.js
+++ src/redux/api.js
@@ -2,19 +2,45 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:3000", credentials: "include" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://127.0.0.1:3000",
+    credentials: "include",
+  }),
   endpoints: (builder) => ({
     getUserPolls: builder.query({
-      query: ({userId, q="all", visibility="all", limit=10, offset=0}) => `/user/poll/${userId}?q=${q}&visibility=${visibility}&limit=${limit}&offset=${offset}`,
+      query: ({
+        userId,
+        q = "all",
+        visibility = "all",
+        limit = 10,
+        offset = 0,
+      }) =>
+        `/user/poll/${userId}?q=${q}&visibility=${visibility}&limit=${limit}&offset=${offset}`,
     }),
     getUserVotedPolls: builder.query({
-      query: ({userId, q="all", visibility="all", limit=10, offset=0}) => `/user/voted/${userId}?q=${q}&visibility=${visibility}&limit=${limit}&offset=${offset}`
+      query: ({
+        userId,
+        q = "all",
+        visibility = "all",
+        limit = 10,
+        offset = 0,
+      }) =>
+        `/user/voted/${userId}?q=${q}&visibility=${visibility}&limit=${limit}&offset=${offset}`,
     }),
     getPolls: builder.query({
-      query: ({q="all", visibility="all", limit=10, offset=0}) => `/poll?q=${q}&visibility=${visibility}&limit=${limit}&offset=${offset}`
-    })
+      query: ({ q = "all", visibility = "all", limit = 10, offset = 0 }) =>
+        `/poll?q=${q}&visibility=${visibility}&limit=${limit}&offset=${offset}`,
+    }),
+    getTrendingPolls: builder.query({
+      query: () => `/poll/trending`,
+    }),
   }),
 });
 
 export { apiSlice };
-export const { useGetUserPollsQuery, useGetUserVotedPollsQuery, useGetPollsQuery } = apiSlice;
+export const {
+  useGetUserPollsQuery,
+  useGetUserVotedPollsQuery,
+  useGetPollsQuery,
+  useGetTrendingPollsQuery,
+} = apiSlice;
